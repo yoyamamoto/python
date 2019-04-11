bf5d4373e05c26c02c9c1fc1fa08d4a520211bc0
yo_yamamoto <ebsw0820e@gmail.com>
2019-04-11 14:20:26 +0900

    test

--- class_sample.py
+++ class_sample.py
@@ -13,5 +13,5 @@ class Class_Sample():
     def __str__(self):
         return 'printした時に実行される特殊関数' + self._var
 
-# これクラスを直接プリントできるんだけどどういう事なんだｗｗｗ
+# これクラスを直接プリントできるんだけどどういう事なんだ。
 print(Class_Sample())
